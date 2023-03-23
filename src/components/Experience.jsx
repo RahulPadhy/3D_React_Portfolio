94e8a312f932381fedc99fb5cf0c92e468f42dba
Rahul Padhy <rahulpadhyrkp@gmail.com>
2023-03-23 23:00:52 +0530

    Experience section completed

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -15,7 +15,7 @@ const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
     contentStyle={{ background: "#1d1836", color: "#fff" }}
     contentArrowStyle={{ borderRight: "7px solid #232631" }}
-    date={experiences.date}
+    date={experience.date}
     iconStyle={{ background: experience.iconBg }}
     icon={
       <div className="flex justify-center items-center w-full h-full">
@@ -29,7 +29,24 @@ const ExperienceCard = ({ experience }) => (
   >
     <div>
       <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+      <p
+        className="text-secondary text-[16px] font-semibold"
+        style={{ margin: 0 }}
+      >
+        {experience.company_name}
+      </p>
     </div>
+
+    <ul className="mt-5 list-disc ml-5 space-y-2">
+      {experience.points.map((point, index) => (
+        <li
+          key={`experience-point-${index}`}
+          className="text-white-100 text-[14px] pl-1 tracking-wider"
+        >
+          {point}
+        </li>
+      ))}
+    </ul>
   </VerticalTimelineElement>
 );
 
